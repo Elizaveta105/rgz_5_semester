7f8778dc6dd3084de75c94a710b6fdcf3a493e6c
Елизавета Якунина <livya2003@mail.ru>
2024-01-05 18:40:25 +0700

    Создание страниц регистрации и входа. Подключение к БД

--- app.py
+++ app.py
@@ -7,3 +7,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+
+app.secret_key = '123'
+user_db = 'user_liza'
+host_ip = '127.0.0.1'
+host_port = '5432'
+database_name = 'elizaveta_rgz'
+password = '123'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(user_id):
+    return users.query.get(int(user_id))
+
+
+@app.route("/")
+def lab6_view():
+    if current_user.is_authenticated:
+        username = current_user.username
+    else:
+        username = "Аноним"
+
+    return render_template("index.html", username=username)
